c6ad0f1342837667796081f21e8f47e8005229f3
dmpossa <dmpossa@gmail.com>
2024-12-31 09:01:26 -0300

    hotfix commit

--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
 int main() {
     printf("Pro Git book examples will be tested here.\n");
     // first commit in main
-    // second commit in main 
+    // second commit in main
+    
+    // this is a hotfix commit 
     return 0;
 }
