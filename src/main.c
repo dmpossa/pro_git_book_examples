525094984997c058fd13d4f05d83de1981fe6f81
dmpossa <dmpossa@gmail.com>
2024-12-31 08:59:11 -0300

    first commit in iss53

--- src/main.c
+++ src/main.c
@@ -4,5 +4,7 @@ int main() {
     printf("Pro Git book examples will be tested here.\n");
     // first commit in main
     // second commit in main 
+    
+    //first commit in branch iss53
     return 0;
 }
