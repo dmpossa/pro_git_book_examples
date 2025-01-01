e79e590724ff671938343a3d3a9dbf70581e81ea
dmpossa <dmpossa@gmail.com>
2025-01-01 16:59:16 -0300

    C5

--- src/main.c
+++ src/main.c
@@ -4,5 +4,6 @@ int main() {
     	printf("Pro Git book examples will be tested here.\n");
         //new work in main, ahad of testing branch
         //sem conflito
+        printf("C5\n");
     return 0;
 }
