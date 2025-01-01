384482c21326d65b17c1528cd70e020c586934af
dmpossa <dmpossa@gmail.com>
2025-01-01 17:03:07 -0300

    clean up

--- src/main.c
+++ src/main.c
@@ -2,9 +2,5 @@
 
 int main() {
     	printf("Pro Git book examples will be tested here.\n");
-        //new work in main, ahad of testing branch
-        //sem conflito
-        printf("C5\n");
-        printf("C6\n");
     return 0;
 }
